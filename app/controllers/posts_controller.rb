5533f9739f0d1b458f627ba2fe878d707ea7645d
lyle dickie <lyle.e.dickie@gmail.com>
2019-10-14 12:43:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
+    binding.pry
     @post = Post.new
     @post.title = params[:title]
     @post.description = params[:description]
